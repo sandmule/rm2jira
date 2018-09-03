fbe33800795fac5fb0965ee03016233afa93fad3
Sam Salter <s.salter@livelinktechnology.net>
2018-09-03 17:11:16 +0100

    updated cli for pdf format

--- lib/rm2jira.rb
+++ lib/rm2jira.rb
@@ -23,10 +23,20 @@ module RM2Jira
       @projects.each_key { |x| puts x }
     end
 
-    desc "migrate_tickets [project_name]", "migrates tickets from redmine to jira"
-    def migrate_tickets(project_name)
+    desc "migrate_tickets [project_name] (ticket_id)", "migrates tickets from redmine to jira. if restarting the application use last ticket id"
+    def migrate_tickets(project_name, ticket_id = 0)
       ticket_ids = RM2Jira::Redmine.get_issue_ids(RM2Jira::Redmine.new.projects[project_name])
-      RM2Jira::Redmine.get_issues(ticket_ids)
+      RM2Jira::Redmine.get_issues(ticket_ids, ticket_id)
+    end
+
+    desc "upload_single_ticket [ticket_id]", "migrates a single tickets from redmine to jira(mainly used for test purposes)"
+    def upload_single_ticket(ticket_id)
+      RM2Jira::Redmine.upload_single_ticket(ticket_id)
+    end
+
+    desc "download_pdfs [project_name]", 'downloads pdfs from redmine'
+    def download_pdfs(project_name)
+
     end
   end
 end
